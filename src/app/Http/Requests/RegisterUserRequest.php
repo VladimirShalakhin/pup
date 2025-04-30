9faf3adf5f491e44e2496b6a7e467460cc691b90
vladimir <shalakhin@kodeks.ru>
2025-04-30 17:09:27 +0300

    - created BaseFormRequest.php - created validation for user registration

--- src/app/Http/Requests/RegisterUserRequest.php
+++ src/app/Http/Requests/RegisterUserRequest.php
@@ -3,19 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
-use Illuminate\Foundation\Http\FormRequest;
 
-class RegisterUserRequest extends FormRequest
+class RegisterUserRequest extends BaseFormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
+     /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, ValidationRule|array|string>
